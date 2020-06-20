3bdf83510e4f8c1f605f2c069217438a6158ab79
Jose Torero <mail@mail.com>
2020-06-20 16:05:36 -0500

    se agregaron los modelos Periodo, Aula, Alumno, Periodo_Aula

--- colegio/educa/admin.py
+++ colegio/educa/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Alumno, Aula, Periodo, Aula_Periodo
 
 # Register your models here.
+
+admin.site.register(Alumno)
+admin.site.register(Aula)
+admin.site.register(Periodo)
+admin.site.register(Aula_Periodo)
--- colegio/educa/models.py
+++ colegio/educa/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+class Aula (models.Model):
+    id = models.AutoField(primary_key = True)
+    grado = models.CharField(max_length = 10)
+    nivel = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.grado + ' ' + self.nivel
+    
+    class Meta:
+        db_table = 'educa_aula'
+        ordering = ['id']
+        verbose_name = 'aula'
+        verbose_name_plural = 'aulas'
+
+class Alumno (models.Model):
+    id = models.AutoField(primary_key = True)
+    aula = models.ForeignKey('Aula', on_delete = models.CASCADE)
+    nombre = models.CharField(max_length = 200)
+    apellido = models.CharField(max_length = 200)
+    edad = models.IntegerField(blank = True, null = True)
+
+    def __str__(self):
+        return self.nombre + ' ' + self.apellido
+
+class Periodo(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre_periodo = models.CharField(max_length = 50)
+    
+
+    def __str__(self):
+        return self.nombre_periodo
+    
+class Aula_Periodo(models.Model):
+    id = models.AutoField(primary_key = True)
+    aula = models.ForeignKey('Aula', on_delete = models.CASCADE)
+    periodo = models.ForeignKey('Periodo', on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.aula.grado  + ' ' + self.aula.nivel + ' ' + self.periodo.nombre_periodo
+
+
+
+
+
+
