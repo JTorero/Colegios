d36ca9e65e8a6b080accde6a99d5decce7d60722
Juan Oscco Mori <juan.osccomori@gmail.com>
2020-06-21 22:16:59 -0500

    se agregaron cambios

--- colegio/educa/models.py
+++ colegio/educa/models.py
@@ -1,36 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import Group
+
 # Create your models here.
 
-class Aula (models.Model):
+class Alumno (models.Model):
     id = models.AutoField(primary_key = True)
-    grado = models.CharField(max_length = 10)
-    nivel = models.CharField(max_length = 50)
+    nombre = models.CharField(max_length = 50)
+    apellido = models.CharField(max_length = 50)
+    edad = models.IntegerField(blank = True)
+    aula = models.ForeignKey('aula', on_delete = models.CASCADE)
 
-    def _str_(self):
-        return self.grado + ' ' + self.nivel
+    def __str__(self):
+        return self.aula.grado + ' ' + self.aula.nivel + ' '+ self.nombre + ' ' + self.apellido 
     
     class Meta:
-        db_table = 'educa_aula'
+        db_table = 'educa_alumno'
         ordering = ['id']
-        verbose_name = 'aula'
-        verbose_name_plural = 'aulas'
+        verbose_name = 'alumno'
+        verbose_name_plural = 'alumnos'
 
 class Periodo(models.Model):
     id = models.AutoField(primary_key = True)
     nombre_periodo = models.CharField(max_length = 50)
 
-    def _str_(self):
+    def __str__(self):
         return self.nombre_periodo
     
-class Aula_Periodo(models.Model):
-    id = models.AutoField(primary_key = True)
-    aula = models.ForeignKey('Aula', on_delete = models.CASCADE)
-    periodo = models.ForeignKey('Periodo', on_delete = models.CASCADE)
-
-    def _str_(self):
-        return self.aula.grado  + ' ' + self.aula.nivel + ' ' + self.periodo.nombre_periodo
-
 class Curso(models.Model):
     id = models.AutoField(primary_key = True)
     nombre_curso = models.CharField(max_length=200, blank=False, null=False)
@@ -40,49 +34,59 @@ class Curso(models.Model):
         verbose_name_plural = 'Cursos'
         ordering = ['nombre_curso']
 
-    def _str_(self):
-        return self.nombre_curso
+    def __str__(self):
+        return self.nombre_curso 
 
-class Curso_docente(models.Model):
+class Aula (models.Model):
     id = models.AutoField(primary_key = True)
-    id_curso = models.ForeignKey('Curso', on_delete = models.CASCADE)
-    docentes = models.ForeignKey('auth.user', on_delete = models.CASCADE)
+    grado = models.CharField(max_length = 10)
+    nivel = models.CharField(max_length = 50)
+    cursos = models.ManyToManyField(Curso)
+    
 
+    def __str__(self):
+        return self.grado + ' ' + self.nivel
+    
     class Meta:
-        verbose_name = 'Curso_Docente'
-        verbose_name_plural = 'Cursos_Docentes'
-        ordering = ['docentes']
-        # users_in_group = Group.objects.get(name="Docente").user_set.all()
-        # if user in users_in_group:
-        #     print(user)
-
-    def _str_(self):
-        return self.id
-
-class Aula_Curso_Docente(models.Model):
-    id = models.AutoField(primary_key=True)
-    aula = models.ForeignKey('Aula', on_delete=models.CASCADE)
-    curso_docente_id = models.ForeignKey('Curso_Docente', on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = 'Aula_Curso_Docente'
-        verbose_name_plural = 'Aulas_Cursos_Docentes'
+        db_table = 'educa_aula'
         ordering = ['id']
+        verbose_name = 'aula'
+        verbose_name_plural = 'aulas'
 
-    def _str_(self):
-        return self.id
+class Docente(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre_docente = models.CharField(max_length=200, blank=False, null=False)
+    apellido_docente = models.CharField(max_length=200, blank=False, null=False)
+    cursos = models.ManyToManyField(Curso)
+
+    def __str__(self):
+        return self.nombre_docente + ' ' + self.apellido_docente
+
 
 class Notas(models.Model):
-    alumno = models.ForeignKey('auth.user', on_delete = models.CASCADE)
-    id_curso = models.ForeignKey('Curso', on_delete = models.CASCADE)
-    id_aula_periodo= models.ForeignKey('Aula_Periodo', on_delete = models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    alumno = models.ForeignKey('Alumno', on_delete = models.CASCADE)
+    curso = models.ForeignKey('Curso', on_delete = models.CASCADE)
+    aula= models.ForeignKey('Aula', on_delete = models.CASCADE, default = alumno)
+    periodo = models.ForeignKey('Periodo', on_delete = models.CASCADE) 
     Nota = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return self.alumno.nombre + ' ' + self.alumno.apellido + ' | ' + self.curso.nombre_curso + ' | ' + ' ' + str(self.Nota)
+
+class Estado_Asistencia(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length=20,blank =True, null = True)
+
+    def __str__(self):
+        return self.nombre
 
 
 class Asistencia(models.Model):
-    alumno = models.ForeignKey('auth.user', on_delete = models.CASCADE)
-    fecha = models.DateField(blank=False, null=False)
-    id_curso_docente = models.ForeignKey('Curso_docente', on_delete = models.CASCADE)
+    alumno = models.ForeignKey('Alumno', on_delete = models.CASCADE)
+    fecha = models.DateTimeField(blank=False, null=False)
+    curso = models.ForeignKey('Curso', on_delete = models.CASCADE)
+    estado_asistencia = models.ForeignKey('Estado_Asistencia', on_delete = models.CASCADE, null = True)
 
-
- Entry.objects.filter(pub_date__gt=datetime.date(2005, 1, 3), headline='Hello')
+    def __str__(self):
+        return self.alumno.nombre + ' ' + self.alumno.apellido + ' ' + str(self.fecha) + ' ' + self.curso.nombre_curso
